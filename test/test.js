dc0edd417e9cfad92f2ad0444fe8a3dd31a4d2e4
christian-bromann <mail@christian-bromann.com>
2016-11-08 17:56:38 +0100

    implemented samsung smart tv class to control TV

---
+++ test/test.js
@@ -0,0 +1,18 @@
+import SamsungSmartTV from '../build/device'
+
+async function main () {
+    const TV = new SamsungSmartTV('192.168.1.2', '5c:49:7d:cc:89:7b')
+    await TV.connect()
+
+    await TV.sendKey('KEY_VOLUP')
+    await TV.sendKey('KEY_VOLUP')
+    await TV.sendKey('KEY_VOLUP')
+    await TV.sendKey('KEY_VOLDOWN')
+    await TV.sendKey('KEY_VOLDOWN')
+    await TV.sendKey('KEY_VOLDOWN')
+
+    await TV.sendKey('KEY_POWER')
+    return TV.disconnect()
+}
+
+main().catch(console.log)
